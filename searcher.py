c09310eed55eec21a5412405cd7a6e593ba4766b
mythical <95757470+namevariable@users.noreply.github.com>
2022-08-02 21:27:32 +0200

    Update searcher.py

--- searcher.py
+++ searcher.py
@@ -1,7 +1,7 @@
 import requests,re
 word = input("Word to search: ")
-def UnistavacVixovogAnusa(raw_html):
+def ilovevany(raw_html):
     cleantext = re.sub(re.compile(r'<.*?>'), '', raw_html)
     return cleantext
 resp=requests.get(f"http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={word}&format=json")
-print(UnistavacVixovogAnusa(resp.json()["query"]["search"][0]["snippet"]))
+print(ilovevany(resp.json()["query"]["search"][0]["snippet"]))
